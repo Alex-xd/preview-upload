cdfd0db49fd9c0e26436ea1d49a7dd3d65471392
zhangboyuan-XD <zhangboyuan_xd@qq.com>
2016-10-26 13:03:24 +0800

    release v1.0.0
    Realization of basic functions.

---
+++ README.md
@@ -0,0 +1,47 @@
+# Image upload Preview
+
+#### A light and easy use of preview a image uploaded. 
+
+## Usage
+
+simply include `imgUploader.js` then 
+
+```javascript
+imgUploader.listen('#uploader');
+```
+
+for example
+
+```html
+<input id="uploader" type="file">
+<div id="preview"></div>
+
+<script src="imgUploader.js"></script>
+<script>
+imgUploader.config({ prvBox:'preview' })
+	       .listen('#uploader');
+</script>
+```
+
+we can put our preview box any where just give it a class or id.
+
+![show](http://o6x2vif88.bkt.clouddn.com/preview-uploader.png)
+
+## More configurations
+
+You can use `config()` to customalize your preview box.
+
+```html
+<script>
+imgUploader.config({
+    prvBox: 'J_preview',
+    width: 200,
+    height: 200,
+    isAbsPosition: true
+}).listen('#uploader');
+</script>
+```
+
+Attribute `isAbsPostion`  means whether to use absolute positioning(set it 'true' to have higher a performance but make the preview box to be a absolute position box).
+
+welcome pr to improve this project & enjoy it.
---
+++ preview.js
@@ -0,0 +1,67 @@
+/**
+ * @author zhangboyuan-XD
+ * @version 1.0.0
+ * @decription image upload with a preview js component.
+ */
+;(function(exports) {
+    /**
+     * default setting
+     * @attribute isAbsPostion  Whether to use absolute positioning（set it 'true' to have a higher performance）
+     * @attribute pvBoxId  The id of a 'div' element which will be put the preview image in.
+     */
+    var options = {
+        width: 150,
+        height: 150,
+        prvBox:'preview',
+        isAbsPostion: false
+    },
+    fr = new FileReader();
+
+    var api = {
+        config: function(opts) {
+            if (!opts) {
+                return options
+            }
+            for (var key in opts) {
+                if (opts.hasOwnProperty(key)) {
+                    options[key] = opts[key];
+                }
+            }
+            return api;
+        },
+        listen: function listen(elem) {
+            if (typeof elem === 'string') {
+                var elems = document.querySelectorAll(elem),
+                    i = elems.length;
+
+                while (i--) {
+                    listen(elems[i]);
+                }
+                return api;
+            }
+
+            elem.onchange = function() {
+                fr.readAsDataURL(elem.files[0]);
+            }
+            api.showPreview(document.getElementById(options.prvBox));
+            return api;
+        },
+        showPreview: function(elem) {
+            fr.onload = function() {
+                var img = document.createElement('img');
+
+                img.style.width = options.width + 'px';
+                img.style.height = options.height + 'px';
+                if (options.isAbsPostion) {
+                    img.style.position = 'absolute';
+                }
+                img.setAttribute('src', fr.result);
+
+                elem.appendChild(img);
+            }
+            return api;
+        }
+    }
+
+    exports.preview = api;
+})(window)
